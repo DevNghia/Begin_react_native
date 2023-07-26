235766ac65d556f086dccc2a3d3c01f7de38c194
unknown <truonggiang01add@gmail.com>
2023-07-26 17:09:51 +0700

    giang day suckhoe +home

--- src/screens/TopPage/items/Home.js
+++ src/screens/TopPage/items/Home.js
@@ -3,6 +3,8 @@ import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
 import MyFlatList from '../../../elements/Flatlist';
 import {Sizes} from '../../../utils/resource';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 
 const HomeScreen = ({navigation}) => {
   const insets = useSafeAreaInsets();
@@ -22,28 +24,23 @@ const HomeScreen = ({navigation}) => {
         }}>
         <TouchableOpacity onPress={() => navigation.navigate('HocPhi')}>
         <View>
-          <Image
+          {/* <Image
             style={styles.icon}
             source={require('../../../utils/Icons/payments.png')}
-          />
+          /> */}
+           <Icon name="payments" size={45} color="#04962D"  style={styles.icon} />
           <Text style={styles.textIcon}>Học phí</Text>
         </View>
         </TouchableOpacity>
         <TouchableOpacity  onPress={() => navigation.navigate('XinNghi')}>
         <View>
-          <Image
-            style={styles.icon}
-            source={require('../../../utils/Icons/edit_note.png')}
-          />
+        <Icon name="email" size={45} color="#FEBE10"  style={styles.icon} />
           <Text style={styles.textIcon}>Xin Nghỉ</Text>
         </View>
         </TouchableOpacity>
         <TouchableOpacity  onPress={() => navigation.navigate('TinTuc')}>
         <View>
-          <Image
-            style={styles.icon}
-            source={require('../../../utils/Icons/newspaper.png')}
-          />
+        <Icon name="article" size={45} color="#08DCEA"  style={styles.icon} />
           <Text style={styles.textIcon}>Tin Tức</Text>
         </View>
         </TouchableOpacity>
@@ -62,28 +59,19 @@ const HomeScreen = ({navigation}) => {
         }}>
         <TouchableOpacity  onPress={() => navigation.navigate('Albums')}>
         <View>
-          <Image
-            style={styles.icon}
-            source={require('../../../utils/Icons/photo_library.png')}
-          />
+        <Icon name="photo-album" size={45} color="#EF0606"  style={styles.icon} />
           <Text style={styles.textIcon}>Albums</Text>
         </View>
         </TouchableOpacity >
         <TouchableOpacity  onPress={() => navigation.navigate('DichVu')}>
         <View>
-          <Image
-            style={styles.icon}
-            source={require('../../../utils/Icons/medical_services.png')}
-          />
+        <Icon name="home-repair-service" size={45} color="black"  style={styles.icon} />
           <Text style={styles.textIcon}>Dịch Vụ</Text>
         </View>
         </TouchableOpacity>
         <TouchableOpacity  onPress={() => navigation.navigate('DanhGia')}>
         <View>
-          <Image
-            style={styles.icon}
-            source={require('../../../utils/Icons/bookmark_add.png')}
-          />
+        <Icon name="assessment" size={45} color="#8F14C9"  style={styles.icon} />
           <Text style={styles.textIcon}>Đánh Giá</Text>
         </View>
         </TouchableOpacity>
@@ -91,7 +79,7 @@ const HomeScreen = ({navigation}) => {
       <View
         style={{
           height: Sizes.device_width < Sizes.device_height,
-          width: 300,
+          width: '90%',
           paddingHorizontal: 15,
           paddingVertical: 20,
           borderRadius: 15,
@@ -112,14 +100,14 @@ const HomeScreen = ({navigation}) => {
       <View
         style={{
           height: Sizes.device_width < Sizes.device_height,
-          width: 300,
+          width: '90%',
 
           paddingHorizontal: 15,
           paddingVertical: 20,
           borderRadius: 15,
           backgroundColor: '#D9D9D9',
           marginVertical: -10,
-          marginHorizontal: 30,
+          marginHorizontal: 15,
           alignItems: 'center',
           justifyContent: 'space-between',
         }}>
@@ -157,6 +145,7 @@ const HomeScreen = ({navigation}) => {
 const styles = StyleSheet.create({
   container: {
     flex:1,
+    alignItems:'center',
     backgroundColor: '#FCEEEE',
   },
   icon: {
